215f2d288b5de87dedc35e170fb95c6a4600059d
vieillem <matthieu.vieille-cessay@edu.esiee.fr>
2024-09-23 13:39:13 +0000

    Voici le code de ma fonction isapalindrome de l'exercice sur les chaîne de caractères

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+'''Aucun module n'est appelé dans ce code là'''
 #### Fonction secondaire
 
 
 def ispalindrome(p):
+    '''Fonction prenant en entrée un mot ou phrase et dit si c'est un palindrome ou non'''
+    table = str.maketrans(
+        'éèàçôêë',    # Caractères à traduire
+        'eeacoee',    # Caractères de remplacement
+        "&(-_)=+}]@^`[{#~,!:;'?./ "  # Caractères à supprimer
+    )
+    translated_text = p.lower().translate(table)
 
-    # votre code ici
-    
-    return False
+    return translated_text == translated_text[::-1]
+
 
 #### Fonction principale
 
@@ -16,6 +23,7 @@ def main():
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
+
 
 
 if __name__ == "__main__":
